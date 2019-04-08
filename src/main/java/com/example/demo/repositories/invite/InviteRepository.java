42dcd0576b3f521e9fa4def0657daad02a0b4d46
Aydar <aydar710@gmail.com>
2019-04-08 20:56:59 +0300

    added repositories

---
+++ src/main/java/com/example/demo/repositories/invite/InviteRepository.java
@@ -0,0 +1,23 @@
+package com.example.demo.repositories.invite;
+
+import com.example.demo.models.Invite;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface InviteRepository extends JpaRepository<Invite, Long> {
+    List<Invite> findAll();
+
+    Invite save(Invite invite);
+
+    Optional<Invite> findById(Long id);
+
+    //TODO: В стаорм проекте передается объект Invite
+    void deleteById(Long id);
+
+    @Query(value = "select * from invite where user_id = :id", nativeQuery = true)
+    List<Invite> getAllUserInvites(@Param("id") Long id);
+}
---
+++ src/main/java/com/example/demo/repositories/project/ProjectRepository.java
@@ -0,0 +1,40 @@
+package com.example.demo.repositories.project;
+
+import com.example.demo.models.Project;
+import com.example.demo.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    List<Project> findAll();
+
+    Project save(Project project);
+
+    Optional<Project> findById(Long id);
+
+    //TODO: Make sure this method works
+    @Query(value = "select users.* from users, user_project where project_id = :id and users.id = user_id",
+            nativeQuery = true)
+    List<User> getAllProjectMembers(@Param("id") Long id);
+
+//    @Query(value = "insert into user_project(project_id, user_id) values (:project.id, :user.id)",
+//            nativeQuery = true)
+//    void addMemberToProject(@Param("user") User user, @Param("project") Project project);
+
+    //TODO: Make sure this method works
+    @Query(value = "insert into user_project(project_id, user_id) values (:projectId, :userId)",
+            nativeQuery = true)
+    void addMemberToProject(@Param("userId") Long userId, @Param("projectId") Project projectId);
+
+    /*TODO: Реализовать все эти методы:
+    getAllAdminsProjects
+    getAllAdmins
+    addAdminToProject
+    getAllMembersProjects
+    findProjectByNameAndMainAdminId*/
+}
